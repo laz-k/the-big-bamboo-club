8839f4d5c73e41650c028f3667032e60e3e00354
laz-k <l.kloukas@yahoo.com>
2021-07-14 07:26:02 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import {AppBar, Toolbar, IconButton} from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+
+
+const Header = () => {
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: "black",
+        boxShadow: "none",
+        padding: "10px 0px"
+      }}
+    >
+      <Toolbar>
+        
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
